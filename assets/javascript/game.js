304fd6f6a53af900a506f79aa14301f9ffdfe4fe
jnoliver88 <jnoliver88@yahoo.com>
2018-10-01 18:38:31 -0500

    added

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//Setting variables //
+
+var won = 0;
+var lost = 0;
+var attempts = 9;
+var usedArray = [];
+var randomLetter = ranLetter;
+var letters = "abcdefghijklmnopqrstuvwxyz"
+
+randomLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(randomLetter);
+
+
