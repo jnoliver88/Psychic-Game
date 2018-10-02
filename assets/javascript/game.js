776375ec05c97378190178176c7c90f07bfa1468
jnoliver88 <jnoliver88@yahoo.com>
2018-10-02 15:56:01 -0500

    I added updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,23 +2,16 @@
 var won = 0;
 var lost = 0;
 var attempts = 9;
-var usedArray = [];
-var ranLetter = ranLetter;
+var usedletters = [];
 var letters = "qwertyuiopasdfghjklzxcvbnm"
 
 
 //GENERATING THE COMPUTERS SECRET LETTER
-//Generates random number (decimal from 0 to 1) which is multiplied by 26 and the rounded down to the nearest whole number by the Math.floor method. 
-//ranLetter = letters[Math.floor(Math.random() * letters.length)];
-//console.log(ranLetter);
-
-//Uses random whole number generated above to select a random letter from the array [letters] and assigns it to the var ranLetter
 function jsGuess() {
-      ranLetter = letters[Math.floor(Math.random() * letters.length)];
-     console.log(ranLetter);
-
+    ranLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(ranLetter);
+    usedletters = [];
 }
-
 jsGuess();
 
 //CAPTURING THE PLAYERS INPUT
@@ -26,50 +19,44 @@ jsGuess();
 document.onkeyup = function (event) {
     var sofar = event.key;
 
+//HANDELING GUESSES - OUTPUT
+//this 'if' prevents a letter selected a 2nd time from being written to the usedletters again, although it still counts as a guess
+    if (usedletters.indexOf(sofar) >= 0) {
 
-if (sofar !== ranLetter) {
-      attempts--;
+    } else {
+        //this pushes the players incorrect guess to the usedletters and writes it to the HTML
+        usedletters.push(sofar);
+        document.getElementById('sofar').innerHTML = usedletters;
+        console.log(usedletters);
 
-}
+    }
 
-      
-     //HANDELING INCORRECT GUESSES
-      //tests if players guess Does Not Equal ranLetter and decriments attempts by 1
-   
-       //when remaining attempts equals zero, lost is incrimented by 1; attempts is reset to 10, and used letters array is cleared
-       if (attempts == 0) {
-        lost++;
-        usedArray = []
+    //HANDELING GUESSES
+    //tests if players guess Does Not Equal ranLetter and decriments attempts by 1
+    if (sofar !== ranLetter) {
+        attempts--;
+    } else {
+        won++;
         attempts = 9;
-  }
+        usedletters = [];
+        jsGuess();
+        alert("You did it!");
+    }
 
-      
-      // HANDELING CORRECT GUESSES
-      //test if players guess equals ranLetter, if true it increments wins by 1, and clears used letters array. Supposed to reset guess # to 10 but starts at 9 instead, 
-      if (sofar === ranLetter) {
-            won++;
-            attempts = 9;
-            usedArray = [];
 
-      }
+    //when remaining attempts equals zero, lost is incrimented by 1; attempts is reset to 10, and used letters array is cleared
+    if (attempts === 0) {
+        lost++;
+        usedletters = [];
+        attempts = 9;
+        jsGuess();
+        alert("Try again");
+    }
 
-     
-    
-      //HANDELING INCORRECT GUESSES - OUTPUT
-      //this 'if' prevents a letter selected a 2nd time from being written to the usedArray again, although it still counts as a guess
-      if (usedArray.indexOf(sofar) >= 0) {
-
-      } else {
-            //this pushes the players incorrect guess to the usedArray and writes it to the HTML
-            usedArray.push(sofar);
-            document.getElementById('sofar').innerHTML = usedArray;
-            console.log(usedArray);
-
-      }
-      //OUTPUT TO HTML
-      //these statements write the values/scores generated to the HTML
-      document.getElementById('wins').innerHTML = won;
-      document.getElementById('losses').innerHTML = lost;
-      document.getElementById('attempts').innerHTML = attempts;
+    //OUTPUT TO HTML
+    //these statements write the values/scores generated to the HTML
+    document.getElementById('wins').innerHTML = won;
+    document.getElementById('losses').innerHTML = lost;
+    document.getElementById('attempts').innerHTML = attempts;
 
 }
